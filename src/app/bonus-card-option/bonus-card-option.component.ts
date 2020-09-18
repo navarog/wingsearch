a39d137a0ffeb4ef9c4c3572be66afabd729e23d
Matej Čief <matej.cief@gmail.com>
2020-09-18 18:28:31 +0200

    Improve bonus card option

--- src/app/bonus-card-option/bonus-card-option.component.ts
+++ src/app/bonus-card-option/bonus-card-option.component.ts
@@ -16,4 +16,16 @@ export class BonusCardOptionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getPointConditions(): { value: string, point: boolean }[][] {
+    return this.card.VP.split(';').reduce((acc, condition) => {
+      const match = condition.match(/[0-9]+\[point\]/)
+
+      if (!match)
+        return acc
+      if (!match.index)
+        return [...acc, [{ value: match[0], point: true }, { value: condition.slice(match[0].length) + ' ', point: false }]]
+      else
+        return [...acc, [{ value: condition.slice(0, match.index), point: false }, { value: match[0], point: true }]]
+    }, [])
+  }
 }
